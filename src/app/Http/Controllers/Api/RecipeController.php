6b30045cf53ebd705e032af88d9ac75dce78136e
Andriy Kuznetsov <andriy.kuznetsov@playwing.net>
2026-05-13 11:14:52 +0300

    Added RabbitMQ queue driver, added RecipeController

---
+++ src/app/Http/Controllers/Api/RecipeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Jobs\SendNotification;
+use App\Models\Recipe;
+use Illuminate\Database\Eloquent\Collection;
+
+class RecipeController extends Controller
+{
+    public function getRecipes(): Collection
+    {
+        SendNotification::dispatch()->onQueue('testqueue');
+
+        return Recipe::with('categories')->get();
+    }
+
+    public function getPopularRecipes(): Collection
+    {
+        return Recipe::with('categories')->limit(5)->get()->sortByDesc('views');
+    }
+}
