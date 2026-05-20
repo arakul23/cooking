e1f4d4dd3a7eb8c80972c4fa4ce90b89eef147c0
Andriy Kuznetsov <andriy.kuznetsov@playwing.net>
2026-05-20 11:36:55 +0300

    Added category page, added author_id for recipe, refactoring

--- src/routes/api.php
+++ src/routes/api.php
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('/recipes')->controller(RecipeController::class)->group(function () {
     Route::get('/', 'getRecipes');
     Route::get('/popular', 'getPopularRecipes');
-    Route::get('/{recipe}', 'show');
+    Route::get('/random', 'getRandomRecipe');
+    Route::get('/{recipe}', 'show')->whereNumber('recipe');
 });
 
 Route::prefix('/categories')->controller(RecipeCategoryController::class)->group(function () {
     Route::get('/', 'index');
     Route::get('/random', 'getRandomCategories');
+    Route::get('/{category}', 'show');
 });
