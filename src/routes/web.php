00ea9971867eb557004d5e1cb894f4188343561e
Andriy Kuznetsov <babls2332@gmail.com>
2026-06-01 19:24:31 +0300

    Added dom crawler

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ParserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/parse', [ParserController::class, 'parse'])->name('parse_recipes');
